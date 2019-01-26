9bae9a964138be048b3b91c10396296f25194ea6
EfremDebretsion <46765782+EfremDebretsion@users.noreply.github.com>
2019-01-25 21:57:01 -0800

    Update calculater02.java

--- src/calculater02.java
+++ src/calculater02.java
@@ -9,8 +9,8 @@ public class calculater02 {
     public static double multiply(double num1, double num2){
         return num1 * num2;
 }
-     public static double multiply(double num1, double num2){
-        return num1 * num2;
+     public static double divaid(double num1, double num2){
+        return num1 / num2;
 }
      
     
